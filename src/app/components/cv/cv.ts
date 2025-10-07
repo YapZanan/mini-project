8d04207a9cd71d796df56a3ef60991052219aa8c
yapzanan <naufalsetiawan64@gmail.com>
2025-10-07 15:30:30 +0700

    add todo list

--- src/app/components/cv/cv.ts
+++ src/app/components/cv/cv.ts
@@ -1,19 +1,50 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Profile } from '../../services/profile';
-import { ProfileModel } from '../../utils/profile.model';
+import { ProfileModel, TodoItem } from '../../utils/profile.model';
 
 @Component({
   selector: 'app-cv',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './cv.html',
   styleUrls: ['./cv.css'],
 })
 export class Cv {
   cvData: ProfileModel;
 
+  newTodo = '';
+  todos: TodoItem[] = [];
+  private nextId = 1;
+
   constructor(private profileService: Profile) {
     this.cvData = this.profileService.getUser();
+  }
+
+  addTodo() {
+    if (this.newTodo.trim()) {
+      this.todos.push({
+        id: this.nextId++,
+        text: this.newTodo.trim(),
+        completed: false,
+      });
+      this.newTodo = '';
+    }
+  }
+
+  removeTodo(id: number) {
+    this.todos = this.todos.filter((todo) => todo.id !== id);
+  }
+
+  toggleTodo(id: number) {
+    const todo = this.todos.find((t) => t.id === id);
+    if (todo) {
+      todo.completed = !todo.completed;
+    }
+  }
+
+  getCompletedCount(): number {
+    return this.todos.filter((todo) => todo.completed).length;
   }
 }
--- src/app/utils/profile.model.ts
+++ src/app/utils/profile.model.ts
@@ -8,3 +8,9 @@ export interface ProfileModel {
   image?: string;
   techStack?: string[];
 }
+
+export interface TodoItem {
+  id: number;
+  text: string;
+  completed: boolean;
+}
