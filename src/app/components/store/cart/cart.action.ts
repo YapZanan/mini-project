f2a4997636df12d82ebd5becb2ce533122614344
yapzanan <naufalsetiawan64@gmail.com>
2025-10-13 14:05:01 +0700

    fix

--- src/app/components/store/cart/cart.action.ts
+++ src/app/components/store/cart/cart.action.ts
@@ -1,7 +1,7 @@
-import {createAction, props} from '@ngrx/store'
-import { CartItem } from './cart.state'
+// import {createAction, props} from '@ngrx/store'
+// import { CartItem } from './cart.state'
 
-export const addToCart = createAction(
-    '[Cart] add Item',
-    props<{item: Omit<CartItem}>
-)
+// export const addToCart = createAction(
+//     '[Cart] add Item',
+//     props<{item: Omit<CartItem}>
+// )
